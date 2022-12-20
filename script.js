c320dcd340cd419af340ed156efea70acf4cf304
Rafael Soriano <rsoriano.insti@gmail.com>
2022-12-20 20:45:38 +0100

    3

--- script.js
+++ script.js
@@ -2,7 +2,25 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
+
+// API para conectar con unsplash.com
+const count = 30;
+const apiKey = "uImVW0oqK2ZVcNjm8Gs3X5IcdNCzF8tj1pCxCPaPMHU";
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//Función que se ejecutará después de cada imagen que se cargue
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
 
 /*************************************************************
  * Aunque me parece un poco excesivo, a fin de aplicar DRY
@@ -18,6 +36,8 @@ function setAttributes(element, attributes) {
 
 //Creamos elementos html para los links y las fotos
 function displayPhotos() {
+    totalImages = photosArray.length;
+    console.log("var totalImages:", totalImages);
     //Para cada objeto en photosArray
     photosArray.forEach((photo)=>{
         //Creamos un <a> por cada foto, para enlazar con la imagen en unsplash.com
@@ -38,17 +58,14 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description
         });
+        //Para ejecutar después de cada imagen que se carga
+        img.addEventListener("load", imageLoaded);
         //Metemos el <img> dentro del <a>, y este dentro de imageContainer
         item.appendChild(img);
         imageContainer.appendChild(item);
     });
 }
 
-
-// API para conectar con unsplash.com
-const count = 10;
-const apiKey = "uImVW0oqK2ZVcNjm8Gs3X5IcdNCzF8tj1pCxCPaPMHU";
-const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 //Obtiene fotos de unsplash.com utilizando su API
 async function getPhotos() {
     try {
@@ -59,6 +76,18 @@ async function getPhotos() {
         // Aquí tratamos el error
     }
 }
+
+/*******************************************************
+ * Cada vez que hacemos scroll comprueba si estamos cerca del final de la página, teniendo
+ * ya todas las imágenes cargadas (ready). Si es así, llama a getPhotos (carga 30 imágenes más)
+********************************************************/ 
+window.addEventListener("scroll", () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight && ready) {
+        getPhotos();
+        ready = false;
+        imagesLoaded = 0
+    }
+});
 
 //ejecutamos getPhotos al cargar la página
 getPhotos();
